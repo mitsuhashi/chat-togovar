e5dc65df46ca9829213d2190c26c2886d122a56e
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-02-17 18:20:52 +0900

    update rs_30.txt

--- pubtator3/get_rs.sh
+++ pubtator3/get_rs.sh
@@ -1,6 +1,4 @@
-# wget https://ftp.ncbi.nlm.nih.gov/pub/lu/PubTator3/mutation2pubtator3.gz
-#cat mutation2pubtator3 | sort -nr | cut -f 3 | grep rs | sort -u | head -10 | sed -E 's/rs0+/rs/' > rs.txt
+# wget https://ftp.ncbi.nlm.nih.gov/pub/lu/PubTator3/mutation2pubtator3.gz in December, 2024
 
-cat mutation2pubtator3 | sort -nr | grep rs | head -30 | sed -E 's/rs0+/rs/' > pubtator_latest_30.txt
-
-cat mutation2pubtator3 | sort -nr | grep rs | cut -f 3 | head -30 | sed -E 's/rs0+/rs/' > rs.txt
+cat mutation2pubtator3 | sort -nr | grep rs | head -31 | sed -E 's/rs0+/rs/' > pubtator_rs_latest_31.txt
+cut -f 3 pubtator_rs_latest_31.txt | sort -u > rs_30.txt
