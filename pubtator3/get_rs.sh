150f30bef945f69c186feee62af46fe366731474
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-03-19 01:22:02 +0900

    update pubtator3

--- pubtator3/get_rs.sh
+++ pubtator3/get_rs.sh
@@ -2,3 +2,10 @@
 
 cat mutation2pubtator3 | sort -nr | grep rs | head -31 | sed -E 's/rs0+/rs/' > pubtator_rs_latest_31.txt
 cut -f 3 pubtator_rs_latest_31.txt | sort -u > rs_30.txt
+
+
+# 後処理
+# 1. dbSNPを検索してrs番号に対応するGene symbolを２列目に記載
+# 2. rs1235072590は、rs80356821にmergeされたため、rs80356821に変更
+#  https://www.ncbi.nlm.nih.gov/snp/rs1235072590
+#  https://www.ncbi.nlm.nih.gov/snp/rs80356821 からGene symbolをHBBを取得
